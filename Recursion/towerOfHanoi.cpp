a6033200db269e35079ec706ed9f2726a821247c
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-10-21 12:41:33 +0530

    mistake in towerofhanoi

--- Recursion/towerOfHanoi.cpp
+++ Recursion/towerOfHanoi.cpp
@@ -10,7 +10,7 @@ void towerOfHanoi(int n,char src,char helper,char dest){
 
 	towerOfHanoi(n-1,src,dest,helper);
 	cout<<"Disk "<<n<<" is transferred from "<<src<<" to "<<dest<<endl;
-	towerOfHanoi(n-1,helper,dest,src);
+	towerOfHanoi(n-1,helper,src,dest);
 }
 
 
